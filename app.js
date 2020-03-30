9ffe4e033e25d3ea5c9aeeeffe6b182d23fef0ee
vic30004 <victor_993@msn.com>
2020-03-29 19:30:29 -0700

    added function to populate selected seats from localstorage

--- app.js
+++ app.js
@@ -6,19 +6,32 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
-const setMovieData = (movieIndex,moviePrice)=>{
-    localStorage.setItem('selectedMovie',movieIndex);
-    localStorage.setItem('selectedMoviePrice',moviePrice);
+populateUi();
+
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovie', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+// Get data from localstorage
+function populateUi(){
+  const selectedSeats= JSON.parse(localStorage.getItem('selectedSeats'));
+  const movieIndex = localStorage.getItem('selectedMovie');
+  const moviePrice = localStorage.getItem('selectedMoviePrice');
+
+  if(selectedSeats !==null && selectedSeats.length>0){
+    seats.forEach((seat,index)=>{
+        if(selectedSeats.indexOf(index)>-1){
+            seat.classList.add('selected')
+        }
+    })
+  }
 }
 
 const updatedSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
   const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-
-  localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
-
-
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
